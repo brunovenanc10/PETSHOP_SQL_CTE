516fc79a55fb25ce5c7d9154b6ca648837802be7
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:21:37 -0300

    Update q_2_02.sql

--- Lista_Subquerys_CTE/q_2_02.sql
+++ Lista_Subquerys_CTE/q_2_02.sql
@@ -1,1 +1,12 @@
-
+WITH media_cliente AS (
+    SELECT 
+        c.id_cliente,
+        c.nome,
+        AVG(co.valor_total) AS media_compra
+    FROM clientes c
+    JOIN compras co ON c.id_cliente = co.id_cliente
+    GROUP BY c.id_cliente, c.nome
+)
+SELECT *
+FROM media_cliente
+WHERE media_compra > 200;
