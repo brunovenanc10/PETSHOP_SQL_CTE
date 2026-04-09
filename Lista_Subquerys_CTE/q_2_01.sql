73e0ea44c33be61c41078df20bb6d98526bb9a59
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:21:12 -0300

    Update q_2_01.sql

--- Lista_Subquerys_CTE/q_2_01.sql
+++ Lista_Subquerys_CTE/q_2_01.sql
@@ -1,1 +1,9 @@
-
+SELECT *
+FROM clientes c
+WHERE c.id_cliente IN (
+    SELECT co.id_cliente
+    FROM compras co
+    JOIN itens_compra ic ON co.id_compra = ic.id_compra
+    JOIN produtos p ON ic.id_produto = p.id_produto
+    WHERE LOWER(p.descricao) LIKE '%ração%'
+);
