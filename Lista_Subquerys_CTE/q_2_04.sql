630801c5f52390e1ccd4379856d70cab1016ae6a
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:22:15 -0300

    Update q_2_04.sql

--- Lista_Subquerys_CTE/q_2_04.sql
+++ Lista_Subquerys_CTE/q_2_04.sql
@@ -1,1 +1,13 @@
-
+WITH total_agendamentos AS (
+    SELECT 
+        s.id_servico,
+        s.nome,
+        COUNT(a.id_agendamento) AS total
+    FROM servicos s
+    LEFT JOIN agendamentos a ON s.id_servico = a.id_servico
+    GROUP BY s.id_servico, s.nome
+)
+SELECT *
+FROM total_agendamentos
+ORDER BY total DESC
+LIMIT 3;
