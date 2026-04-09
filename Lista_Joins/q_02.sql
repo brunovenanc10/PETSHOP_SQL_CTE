f742bbe1c9fbe017da4c55181507497a6ad53974
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:23:57 -0300

    Update q_02.sql

--- Lista_Joins/q_02.sql
+++ Lista_Joins/q_02.sql
@@ -1,1 +1,15 @@
-
+SELECT 
+    c.nome AS nome_cliente,
+    v.data_venda,
+    pr.nome AS nome_produto,
+    iv.quantidade,
+    iv.preco_unitario,
+    (iv.quantidade * iv.preco_unitario) AS subtotal
+FROM vendas v
+JOIN clientes c 
+    ON v.id_cliente = c.id_cliente
+JOIN itens_venda iv 
+    ON v.id_venda = iv.id_venda
+JOIN produtos pr 
+    ON iv.id_produto = pr.id_produto
+ORDER BY v.data_venda DESC;
