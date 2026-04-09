d7015488b539b1a61dc2a28c328fd6d26a04ede6
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:22:43 -0300

    Update q_2_05.sql

--- Lista_Subquerys_CTE/q_2_05.sql
+++ Lista_Subquerys_CTE/q_2_05.sql
@@ -1,1 +1,24 @@
-
+WITH total_cliente AS (
+    SELECT 
+        c.id_cliente,
+        c.nome,
+        SUM(co.valor_total) AS total_gasto
+    FROM clientes c
+    JOIN compras co ON c.id_cliente = co.id_cliente
+    GROUP BY c.id_cliente, c.nome
+),
+media_geral AS (
+    SELECT AVG(total_gasto) AS media FROM total_cliente
+),
+clientes_com_pets AS (
+    SELECT 
+        p.id_cliente,
+        COUNT(p.id_pet) AS qtd_pets
+    FROM pets p
+    GROUP BY p.id_cliente
+)
+SELECT tc.*
+FROM total_cliente tc
+JOIN media_geral mg ON tc.total_gasto > mg.media
+JOIN clientes_com_pets cp ON tc.id_cliente = cp.id_cliente
+WHERE cp.qtd_pets > 1;
