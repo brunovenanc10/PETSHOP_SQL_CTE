7611402ec565f7807191ff44bbccea8350342667
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:24:20 -0300

    Update q_04.sql

--- Lista_Joins/q_04.sql
+++ Lista_Joins/q_04.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    p.id_produto,
+    p.nome,
+    COALESCE(SUM(iv.quantidade), 0) AS total_vendido
+FROM produtos p
+LEFT JOIN itens_venda iv 
+    ON p.id_produto = iv.id_produto
+GROUP BY p.id_produto, p.nome
+ORDER BY total_vendido DESC;
