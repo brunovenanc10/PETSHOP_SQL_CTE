3d0f682d0e598219d5e89cc4614e6d127d2bbec4
Bruno Venancio <brunin207.s@gmail.com>
2026-04-09 16:24:10 -0300

    Update q_03.sql

--- Lista_Joins/q_03.sql
+++ Lista_Joins/q_03.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    c.id_cliente,
+    c.nome,
+    COALESCE(COUNT(v.id_venda), 0) AS total_compras
+FROM clientes c
+LEFT JOIN vendas v 
+    ON c.id_cliente = v.id_cliente
+GROUP BY c.id_cliente, c.nome
+ORDER BY total_compras DESC;
